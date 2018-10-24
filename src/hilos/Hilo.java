7387d305375db667090b8fa258bd2b4ecac934f7
Alberto Tomadin <albertodtomadin@yahoo.com>
2018-10-24 20:29:49 -0300

    chat para seguir revisando

---
+++ src/hilos/Hilo.java
@@ -0,0 +1,36 @@
+package hilos;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+import codigo.Servidor;
+
+public class Hilo extends Thread{
+	private boolean suspender = false;
+	private int port;
+	private Servidor servidor;
+	
+	public Hilo(int port, Servidor server) {
+		this.port = port;
+		this.servidor = server;
+	}
+	
+	public void run() {
+		while (suspender) {
+			try {
+				servidor.levantarConexion(port);
+				servidor.flujos();
+				servidor.recibirDatos();
+			} finally {
+				servidor.cerrarConexion();
+			}
+		}
+		
+		suspender = false;
+	}
+	
+	public void suspederHilo() {
+		suspender = true;
+	}
+	
+}
